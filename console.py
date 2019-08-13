2d351df3e59e45cc85e8882e995845d4310aa687
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-08-13 15:30:09 -0400

    and int and float handling

--- console.py
+++ console.py
@@ -49,6 +49,8 @@ class HBNBCommand(cmd.Cmd):
                 if type(item_type) is str:
                     listitem[1] = listitem[1].replace('_', ' ')
                     var = listitem[1][1:-1].replace('"', '\\"')
+                    setattr(obj, listitem[0], var)
+                elif type(item_type) is int or type(item_type) is float:
                     setattr(obj, listitem[0], eval(listitem[1]))
             obj.save()
             print("{}".format(obj.id))
