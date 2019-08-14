707d03d4d98b98437988ad62a10d6a7e6bbdbf1f
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-08-14 14:22:14 -0400

    pep8 fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -44,4 +44,20 @@ class DBStorage(BaseModel, Base):
 
     def save(self):
         """save all changes"""
+
         self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete object from database"""
+
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        City = models.city.City()
+        State = models.state.State()
+        models.base_model.Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+        
