dbaca215854ce31341d83d7e63b2842e7501ce8e
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-08-15 16:26:30 -0400

    update to base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,12 @@
 import uuid
 import models
 from datetime import datetime
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+fmt = '"%Y-%m-%dT%H:%M:%S.%f"'
 
 
 class BaseModel:
@@ -20,28 +26,17 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
-        if kwargs and "id" not in self.__dict__:
+        if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if (key == "created_at" or key == "updated_at") and key != "__class__":
+                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
-                    setattr(self, key, value)
-            if "id" not in kwargs:
-                self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
-        elif kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-            if "id" not in kwargs:
-                self.id = str(uuid.uuid4())
+                    setattr(self, key, time)
+            if key != "created_at" and key != "updated_at" and kwy != "__class__":
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.upsated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
