360f52cfed418ad2636fbd48aa0c7eaaf21b1804
DracoMindz <DracoMindz@github.com>
2019-08-20 01:38:27 +0000

    Compressed tgz archive with datetime format

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""fabric script to generate a .tgz archive from web_static"""
+from fabric.api import *
+import datetime
+
+
+def do_pack():
+    """
+    Stores contents of web_static in compressed archive.
+    Return the archive path if the archive has been correctly generated.
+    Otherwise, it should return None
+
+    """
+    local('mkdir -p versions')
+    time = datetime.datetime.strftime('%Y%m%d%H%M%S')
+    result = local('tar -czvf versions/web_static_{}.tgz web_static)'
+                   .format(time))
+    if result.failed:
+        return None
+    else:
+        return result
