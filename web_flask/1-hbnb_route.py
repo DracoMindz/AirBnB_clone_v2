82ff65d05507c7aa1ab70b3b0b9c788f9c644371
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-04 17:52:47 -0400

    Flask: removed whitespace

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" 
-States Rules for accessing parts of the web 
+"""
+States Rules for accessing parts of the web
 """
 
 from flask import Flask
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def hello_hbnb():
     """ prints Hello HBNB """
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
