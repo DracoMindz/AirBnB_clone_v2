56f128cbae40474a7188513a810e126d5a4bf89e
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-10-10 23:50:43 -0400

    Update

--- models/state.py
+++ models/state.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """This is the state class"""
 import os
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Float
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    __tablename__ = "states"
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(string(128), nullable=False)
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
         cities = relationship("City",
-                              cascade="all, delete-orphan",
+                              cascade="delete-orphan",
                               back_populates="state")
 
     else:
